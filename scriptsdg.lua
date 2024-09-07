7757121e464a14858672995bb24aea2d12c2abee
Lurique25 <mcmclhmm2@gmail.com>
2024-09-07 01:24:59 -0300

    Update scriptsdg.lua

--- scriptsdg.lua
+++ scriptsdg.lua
@@ -1,8 +1,26 @@
-UI.Label("discord: riquezerah")
-UI.Label("discord: riquezerah")
-UI.Label("discord: riquezerah")
-UI.Label("discord: riquezerah")
-UI.Label("discord: riquezerah")
+setDefaultTab("A")
+macro(200. funtion()
+  error444
+setDefaultTab("b")
+  macro(200. funtion()
+  error444
+setDefaultTab("c")
+    macro(200. funtion()
+  error444
+setDefaultTab("d")
+      macro(200. funtion()
+  error444
+setDefaultTab("e")
+        macro(200. funtion()
+  error444
+setDefaultTab("f")
+          macro(200. funtion()
+  error444
+setDefaultTab("h")
+            macro(200. funtion()
+  error444
+setDefaultTab("i")
+
 UI.Label("discord: riquezerah")
 UI.Label("discord: riquezerah")
 UI.Label("discord: riquezerah")
